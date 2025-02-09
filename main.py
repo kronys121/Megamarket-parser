f086ffb6352ebcfd2c459a0f00276ba8230ba3a0
Александр <kronys12@gmail.com>
2025-02-09 22:19:52 +0500

    + add Bonus Percent and Bonus amount

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium_stealth import stealth
 
@@ -24,9 +23,9 @@ def stealth_driver():
 
 driver = stealth_driver()
 driver.get('https://megamarket.ru/')
-
+# Находим элемент поиска и вставляем текст
 time.sleep(4)
-search_but = driver.find_element(By.CLASS_NAME, 'search-tab')
+search_but = driver.find_element("class name", 'search-tab')
 search_but.click()
 input_text = driver.find_element("class name", "search-input__textarea")
 input_text.send_keys(search_text)
@@ -36,6 +35,7 @@ click_but.click()
 time.sleep(4)
 check_boxs = driver.find_elements("class name", "pui-checked-control-item__label.pui-toggle__label")
 
+# Проверка на кол-во чек боксов и выбор Товаров в наличии
 lst_elem = []
 for elements in check_boxs:
     lst_elem.append(elements)
@@ -45,7 +45,7 @@ if len(lst_elem) == 6:
 else:
     available_goods = check_boxs[2]
     available_goods.click()
-print(len(lst_elem))
+
 time.sleep(4)
 driver.find_element("class name", "input.text-input.size").click()
 elements_class = driver.find_elements("class name", "option")
@@ -57,24 +57,25 @@ time.sleep(6)
 main_page = BeautifulSoup(driver.page_source, "lxml")
 
 content = main_page.find("div",class_="catalog-items-list__container")
-
 content_id = content.find_all("div", class_="catalog-item-regular-desktop")
+
+#Поиск и вывод элементов Цены, названия, средней оценки и кол-во комментариев
 for name_id in content_id:
+    bonus_price = "0%"
+    bonus_amount = "0"
     try:
         price = name_id.find("div", class_="catalog-item-regular-desktop__price-conditions").find("div",class_="catalog-item-regular-desktop__price-block").find("div", class_="catalog-item-regular-desktop__price").get_text(strip=True)
+        try:
+            bonus_amount = name_id.find("div", class_="catalog-item-regular-desktop__price-conditions").find("div",class_="catalog-item-regular-desktop__price-block").find("div",class_="catalog-item-regular-desktop__bonus money-bonus sm money-bonus_loyalty").find("span", class_="bonus-amount").get_text(strip=True)
+
+            bonus_price = name_id.find("div", class_="catalog-item-regular-desktop__price-conditions").find("div",class_="catalog-item-regular-desktop__price-block").find("div",class_="catalog-item-regular-desktop__bonus money-bonus sm money-bonus_loyalty").find("span", class_="bonus-percent").get_text(strip=True)
+        except AttributeError:
+            pass
         name = name_id.find("div", class_= "catalog-item-regular-desktop__main-info").find("a").get("title")
         comments_rate = name_id.find("div", class_= "catalog-item-review").find("a", class_="catalog-item-review__link").find("div", class_= "catalog-item-review__review-wrapper").find("div", class_= "pui-rating-display").find("div", class_= "pui-rating-display__rating").find("span").get_text(strip=True)
         comments_rate_count = name_id.find("div", class_="catalog-item-review").find("a", class_="catalog-item-review__link").find("div", class_="catalog-item-review__review-wrapper").find("div", class_="pui-rating-display").find("div",class_="pui-rating-display__text").get_text(strip=True)
-        print(f"Название товара: {name} Цена: {price} Средня оценка: {comments_rate} Кол-во комментариев: {comments_rate_count}")
+        print(f"Название товара: {name} Цена: {price} Скидка: {bonus_price} Бонусные рубли: {bonus_amount} Средня оценка: {comments_rate} Кол-во комментариев: {comments_rate_count}")
     except AttributeError:
         print("Скорее всего товара нет в наличии")
-
-
-
-
-
-
-
-
 
 time.sleep(50)
